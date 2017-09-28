55ef8ea7e4ec516adc26254b68d8639373726de6
josdan <danielyapura@gmail.com>
2017-09-28 15:08:50 -0300

    se creo la clase Requerimiento del paquete Dominio

---
+++ PHC/src/dominio/Requerimiento.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author josdan
+ */
+public class Requerimiento {
+    private int idRequerimiento;
+    private String nombre;
+
+    public Requerimiento(int idRequerimiento, String nombre) {
+        this.idRequerimiento = idRequerimiento;
+        this.nombre = nombre;
+    }
+
+    public int getIdRequerimiento() {
+        return idRequerimiento;
+    }
+
+    public void setIdRequerimiento(int idRequerimiento) {
+        this.idRequerimiento = idRequerimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
