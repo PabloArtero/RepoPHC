2d76cc009a6f0ef1cbb80e196093d1e1ba636f4f
josdan <danielyapura@gmail.com>
2017-09-28 15:10:38 -0300

    Se creo la clase Tarea del paquete Dominio

---
+++ PHC/src/dominio/Tarea.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author josdan
+ */
+public class Tarea {
+    private int idTarea;
+    private String descripcion;
+
+    public Tarea(int idTarea, String descripcion) {
+        this.idTarea = idTarea;
+        this.descripcion = descripcion;
+    }
+
+    public int getIdTarea() {
+        return idTarea;
+    }
+
+    public void setIdTarea(int idTarea) {
+        this.idTarea = idTarea;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
