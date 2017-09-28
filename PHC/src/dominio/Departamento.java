ccaf9937ceb5b1846a760e595686ca4cf681f1a4
josdan <danielyapura@gmail.com>
2017-09-28 15:07:06 -0300

    se creo la clase Departamento del paquete Dominio

---
+++ PHC/src/dominio/Departamento.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author josdan
+ */
+public class Departamento {
+    
+    private int idDepartamento;
+    private String nombre;
+
+    public Departamento(int idDepartamento, String nombre) {
+        this.idDepartamento = idDepartamento;
+        this.nombre = nombre;
+    }
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
