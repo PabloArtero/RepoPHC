cab53eb132ae3e2aabee2f398233a7d58c3a764c
PabloArtero <pabloartero1@gmail.com>
2017-10-02 19:43:16 -0300

    Modificada la case MysqlNivelDAO
    Antes obtenía la conexión por un parámetro en el constructor. Ahora ya la tiene incorporada porque extiende a la clase Conexion.

--- PHC/src/persistencia/MYSQL/MysqlNivelDAO.java
+++ PHC/src/persistencia/MYSQL/MysqlNivelDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package persistencia.MYSQL;
 
 import dominio.Nivel;
@@ -11,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,18 +16,20 @@ import persistencia.NivelDAO;
  *
  * @author josdan
  */
-public class MysqlNivelDAO implements NivelDAO{
+public class MysqlNivelDAO extends Conexion implements NivelDAO{
+    
+    //private Connection conexion = null;
     
     final String INSERT = "INSERT INTO nivel(nombre) VALUES (?)";
     final String UPDATE = "UPDATE nivel SET nombre = ? WHERE idNivel = ?";
     final String DELETE = "DELETE FROM nivel WHERE idNivel = ?";
     final String GETALL = "SELECT * FROM nivel";
     final String GETONE = "SELECT * FROM nivel WHERE idNivel=?";
-    
-    private Connection conexion;
 
-    public MysqlNivelDAO(Connection conexion) {
-        this.conexion = conexion;
+    public MysqlNivelDAO() {
+        
+        ///PRUEBAS
+        //ejecutarPruebas();
     }
     
     private Nivel convertir(ResultSet resultSet) throws SQLException {
@@ -49,12 +47,14 @@ public class MysqlNivelDAO implements NivelDAO{
     
     @Override
     public Nivel obtener(int id) {
+        Connection conexion = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         
         Nivel nivel = null;
         
         try{
+            conexion = getConexion();
             statement = conexion.prepareStatement(GETONE);
             statement.setInt(1, id);
             resultSet = statement.executeQuery();
@@ -68,23 +68,25 @@ public class MysqlNivelDAO implements NivelDAO{
             try {
                 statement.close();
                 resultSet.close();
+                conexion.close();
             } catch (SQLException ex) {
                 Logger.getLogger(MysqlNivelDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         }
         
         return nivel;
     }
 
     @Override
-    public List<Nivel> obtenerTodos() {
+    public ArrayList<Nivel> obtenerTodos() {
+        Connection conexion = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         
-        List<Nivel> niveles = new ArrayList<>();
+        ArrayList<Nivel> niveles = new ArrayList<>();
        
         try{
+            conexion = getConexion();
             statement = conexion.prepareStatement(GETALL);
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -96,10 +98,10 @@ public class MysqlNivelDAO implements NivelDAO{
             try {
                 statement.close();
                 resultSet.close();
+                conexion.close();
             } catch (SQLException ex) {
                 Logger.getLogger(MysqlNivelDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         }
         
         return niveles;
@@ -107,13 +109,15 @@ public class MysqlNivelDAO implements NivelDAO{
 
     @Override
     public void eliminar(Nivel nivel) {
+        Connection conexion = null;
         PreparedStatement statement = null;
         
         try{
+            conexion = getConexion();
             statement = conexion.prepareStatement(DELETE);
             statement.setInt(1, nivel.getIdNivel());
             if (statement.executeUpdate() == 0 ) {
-                System.out.append("el alumno no a sido borrado porque no existe");
+                System.out.append("el registro no fué borrado porque no existe");
             }
             
         } catch (SQLException ex) {
@@ -121,23 +125,25 @@ public class MysqlNivelDAO implements NivelDAO{
         } finally {
             try {
                 statement.close();
+                conexion.close();
             } catch (SQLException ex) {
                 Logger.getLogger(MysqlNivelDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         }
     }
 
     @Override
     public void modificar(Nivel nivel) {
+        Connection conexion = null;
         PreparedStatement statement = null;
         
         try{
+            conexion = getConexion();
             statement = conexion.prepareStatement(UPDATE);
             statement.setString(1, nivel.getNombre());
             statement.setInt(2, nivel.getIdNivel());
             if (statement.executeUpdate() == 0 ) {
-                System.out.append("el alumno no a sido modificado");
+                System.out.append("el registro no fué modificado");
             }
             
         } catch (SQLException ex) {
@@ -145,6 +151,7 @@ public class MysqlNivelDAO implements NivelDAO{
         } finally {
             try {
                 statement.close();
+                conexion.close();
             } catch (SQLException ex) {
                 Logger.getLogger(MysqlNivelDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -154,12 +161,11 @@ public class MysqlNivelDAO implements NivelDAO{
 
     @Override
     public void insertar(Nivel nivel) {
-
+        Connection conexion = null;
         PreparedStatement statement = null;
-        ResultSet res = null;
         
         try {
-            
+            conexion = getConexion();
             statement = conexion.prepareStatement(INSERT);
             statement.setString(1, nivel.getNombre());
             statement.executeUpdate();
@@ -171,11 +177,52 @@ public class MysqlNivelDAO implements NivelDAO{
             if(statement != null)
             try {
                 statement.close();
+                conexion.close();
             } catch (SQLException ex) {
                 Logger.getLogger(MysqlNivelDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-      
+    } 
+
+///PRUEBAS
+    private void ejecutarPruebas() {
+        System.out.println("Pruebas de MysqlNivelDAO");
+        //pruebaInsertar();
+        //pruebaObtener();
+        //pruebaObtenerTodos();
+        //pruebaEliminar();
+        //pruebaModificar();
     }
-    
+   
+    private void pruebaInsertar(){
+        System.out.println("Prueba de Insertar Nivel");
+        Nivel n = new Nivel("Nivel 1");
+        this.insertar(n);
+    }
+    private void pruebaObtener(){
+        System.out.println("Prueba de Obtener Nivel");
+        Nivel n = this.obtener(1);
+        System.out.println("    "+n);
+    }
+    private void pruebaObtenerTodos(){
+        System.out.println("Prueba de Obtener Todos los niveles");
+        ArrayList<Nivel> niveles = this.obtenerTodos();
+        Iterator i = niveles.iterator();
+        while(i.hasNext()){
+            System.out.println("  "+i.next());
+        }
+    }
+    private void pruebaEliminar(){
+        System.out.println("Prueba de Eliminar nivel");
+        Nivel n = this.obtener(4);
+        this.eliminar(n);
+        this.eliminar(n);
+    }
+    private void pruebaModificar(){
+        System.out.println("Prueba de Modificar nivel");
+        Nivel n = this.obtener(1);
+        n.setNombre(n.getNombre()+" MODIFICADO");
+        this.modificar(n);
+        System.out.println(n);
+    }
 }
